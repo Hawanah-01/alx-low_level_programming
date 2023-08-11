0dac4937d50e91c06b800b11ebde0aea63fb2c74
Hawanah <olawaleawanat@gmail.com>
2023-08-10 23:05:30 -0700

    Hysterical

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include <stdio.>
+#include <stdio.h>
 /**
- * main - main block
- * return: 0
- */
-
+* main - printing output with printf
+* Return: 0
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);	
+	return (0);
 }
