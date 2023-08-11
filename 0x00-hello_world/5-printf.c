afd4a1429d9d9e4e9f4670421aa44ebbf01a866c
Hawanah <olawaleawanat@gmail.com>
2023-08-10 20:54:06 -0700

    A new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #!/bin/bash
-#incude <stdio.h>
+#incude < stdio.h >
 /**
  * main - main block
  * return: 0
  */
 int main(void)
 	{
-		printf("with proper grammar, but the outcome is a piece of art,");
-		return: 0;
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return: 0;
 	}
