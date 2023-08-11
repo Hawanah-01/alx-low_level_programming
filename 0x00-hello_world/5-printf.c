7909f0ebd0f34d02f6db3e5cc9a9a6bde3d59c7a
Hawanah <olawaleawanat@gmail.com>
2023-08-10 22:10:55 -0700

    New

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#!/bin/bash
-#incude < stdio.h >
+#include <stdio.>
 /**
  * main - main block
  * return: 0
  */
 int main(void)
 	{
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return: 0;
+		printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);	
 	}
