82d0dbf1757afaf6e32e134fc6e8cc71e8d3cf47
Hawanah <olawaleawanat@gmail.com>
2023-09-11 19:37:39 -0700

    Indispensable

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef dog
-#define dog
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - dog class
@@ -23,5 +23,8 @@ typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
+
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
