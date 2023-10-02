338599b7ae4a52cb82460153362055c715002886
Hawanah-01 <olawaleawanat@gmail.com>
2023-10-02 10:32:28 -0700

    New name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a function
+ * @argc: first argument
+ * @argv: second argument
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
