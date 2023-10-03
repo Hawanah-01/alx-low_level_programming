7e8b8150c7f45de14cd216e71515b82fcc7e363a
Hawanah-01 <olawaleawanat@gmail.com>
2023-10-02 20:36:12 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: first argument
+ * @argv: second argument
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
