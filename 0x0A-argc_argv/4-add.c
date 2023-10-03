1b644cf428e267cab99db9f80f00b2adbf7ab0f6
Hawanah-01 <olawaleawanat@gmail.com>
2023-10-02 21:03:15 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * main - function that checks if a number is digit
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum, i, j;
+
+	sum = 0;
+	if (argc != 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; j < (int)strlen(argv[i]); j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
