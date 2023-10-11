e8a42cc53631c47dd1ee146931f150723b2cc870
Hawanah-01 <olawaleawanat@gmail.com>
2023-10-11 06:27:05 -0700

    Dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - stores info about a particular dog
+ * @name:  dog's name
+ * @age: dog's age
+ * @owner: dog's owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG.H*/
