b5d20466e84bfe2970507bbfd711c9bcd07945ce
Hawanah-01 <olawaleawanat@gmail.com>
2023-10-26 06:57:11 -0700

    Get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 	one = 1 << index;
-	two = n & div;
+	two = n & one;
 	if (two == one)
 		return (1);
 
